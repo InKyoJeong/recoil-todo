75bb6fee04bd22b0139b47c3423f20006f43b584
InKyoJeong <viapolar@naver.com>
2021-03-07 01:00:35 +0900

    add todo list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { RecoilRoot } from "recoil";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <RecoilRoot>
-      <App />
+      <TodoList />
     </RecoilRoot>
   );
 }
